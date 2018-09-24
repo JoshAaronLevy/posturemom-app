a3289e60ce955969535de9dde848f7f9076621c1
Josh <joshaaronlevy@gmail.com>
2018-09-24 09:45:32 -0600

    Trying to get webcam to fire up on page.

---
+++ src/pages/Dashboard/Session.vue
@@ -0,0 +1,30 @@
+<template>
+<div>
+  <div id="info" style='display:none'>
+  </div>
+  <div id="loading">
+      Loading the model...
+  </div>
+
+  <div id='main' style='display:none'>
+      <video id="video" playsinline style=" -moz-transform: scaleX(-1);
+      -o-transform: scaleX(-1);
+      -webkit-transform: scaleX(-1);
+      transform: scaleX(-1);
+      display: none;
+      ">
+      </video>
+      <canvas id="output" />
+  </div>
+</div>
+</template>
+
+<script>
+import * as posenet from '@tensorflow-models/posenet';
+// import dat from 'dat.gui';
+// import Stats from 'stats.js';
+import { drawKeypoints, drawSkeleton, drawBoundingBox } from '../../demo_util';
+
+export default {};
+</script>
+
