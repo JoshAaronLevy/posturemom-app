163ce0567bdd8f059ff6a56473364ed179e8ba1a
Josh <joshaaronlevy@gmail.com>
2018-09-24 14:41:24 -0600

    Added static alert for presentation

---
+++ src/pages/Dashboard/Alerts.vue
@@ -0,0 +1,25 @@
+<template>
+  <b-card>
+    <b-media no-body>
+      <b-media-aside vertical-align="center">
+        <b-img src="https://s3-us-west-2.amazonaws.com/posturemom/mom.png" height="350" alt="mom" />
+      </b-media-aside>
+      <b-media-body class="ml-3 quote-box">
+        <h3 class="mt-0">"Sit up straight, Quasimodo..."</h3>
+      </b-media-body>
+    </b-media>
+  </b-card>
+</template>
+
+<style scoped>
+.quote-box {
+  display: flex;
+  height: 350px;
+}
+.quote-box h3 {
+  display: flex;
+  align-items: center;
+  font-weight: 500;
+}
+</style>
+
